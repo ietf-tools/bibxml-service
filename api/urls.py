9962eb66d317de89dce29bacf1054e0a37147b3b
froot <vladimir.yablokov@gmail.com>
2021-08-16 14:53:41 +0700

    added endpoints, WIP

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from django.urls import include, path, re_path
+
+from . import views
+
+_libs = ["rfc", "id", "std", "bcp", "fyi"]
+re_libs = f'(?P<lib>{"|".join(_libs)})'
+
+urlpatterns = [
+    path('v1/', views.index, name='api_index'),
+    path('v1/search', views.search, name='api_search'),
+    re_path(f'^v1/ref/{re_libs}/(?P<ref>[A-z0-9\_\-\.]+)/', views.get_ref, name='api_get_ref'),
+]
+
---
+++ api/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse, JsonResponse
+
+
+def index(request):
+    return HttpResponse("API v1 index, use search or ref")
+
+def search(request):
+    # TODO: 
+    # implement pasing json request for search fields
+    # implement search
+
+    if request.method == 'POST':
+        return JsonResponse({
+            "results":  []
+        })        
+    else:
+        return JsonResponse({
+                "error": "Bad HTTP method"
+            },
+            status=405
+        )
+
+def get_ref(request, lib, ref):
+    # TODO:
+    # implement getting referense from lib by lib, ref
+    # implement convertation 
+
+    return JsonResponse({
+        "library": lib,
+        "ref": ref
+    })
