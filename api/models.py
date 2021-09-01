2b11e54f351cb046530bd5c5a2d9b16ea31009d5
froot <vladimir.yablokov@gmail.com>
2021-09-02 04:37:20 +0700

    sync bib data model with indexer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BibData(models.Model):
+    bib_id = models.CharField(max_length=64)
+    bib_type = models.CharField(max_length=16)
+    body = models.JSONField("body")
+
+    class Meta:
+        managed = False
+        db_table = 'bib_data'
+        unique_together = [['bib_id', 'bib_type']]
