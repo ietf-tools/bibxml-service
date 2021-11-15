8593a84b5f6ed5c43fc2de95421aabd69c64df79
Anton Strogonoff <anton@strogonoff.name>
2021-11-15 20:19:37 +0100

    chore(models): add single `ref` field

--- api/models.py
+++ api/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class RefData(models.Model):
+    # Reference (or ID). Corresponds to source dataset filename without extension
+    ref = models.CharField(max_length=128)
+
+    # DEPRECATED: Use ref
     ref_id = models.CharField(max_length=64)
+
+    # DEPRECATED: Use ref
     ref_type = models.CharField(max_length=24)
 
     # Matches ID in settings.RELATON_DATASETS
