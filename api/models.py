f1116996c78c5c5e7570f28444798c24d9b35345
Anton Strogonoff <anton@strogonoff.name>
2021-11-15 20:20:04 +0100

    chore(models): describe body field

--- api/models.py
+++ api/models.py
@@ -14,6 +14,7 @@ class RefData(models.Model):
     # Matches ID in settings.RELATON_DATASETS
     dataset = models.CharField(max_length=24)
 
+    # Contains canonical Relaton representation
     body = models.JSONField()
 
     class Meta:
