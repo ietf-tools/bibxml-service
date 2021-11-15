9e8d789a21d13a8e3fc73626ea24f49d3d399a37
Anton Strogonoff <anton@strogonoff.name>
2021-11-15 20:23:35 +0100

    chore(models): use proper docstring syntax

--- api/models.py
+++ api/models.py
@@ -2,23 +2,26 @@ from django.db import models
 
 
 class RefData(models.Model):
-    # Reference (or ID). Corresponds to source dataset filename without extension
-    ref = models.CharField(max_length=128)
+    ref = models.CharField(
+        max_length=128,
+        help_text="Reference (or ID). Corresponds to source dataset filename without extension.")
 
-    # DEPRECATED: Use ref
     ref_id = models.CharField(max_length=64)
+    """DEPRECATED: Use ref"""
 
-    # DEPRECATED: Use ref
     ref_type = models.CharField(max_length=24)
+    """DEPRECATED: Use ref"""
 
-    # Matches ID in settings.RELATON_DATASETS
-    dataset = models.CharField(max_length=24)
+    dataset = models.CharField(
+        max_length=24,
+        help_text="Internal dataset ID.")
+    """Matches ID in settings.RELATON_DATASETS."""
 
-    # Contains canonical Relaton representation
     body = models.JSONField()
+    """Contains canonical Relaton representation"""
 
-    # Contains a mapping of { format: string }, where format is e.g. bibxml
     representations = models.JSONField()
+    """Contains a mapping of { format: string }, where format is e.g. bibxml"""
 
     class Meta:
         db_table = 'api_ref_data'
