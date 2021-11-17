450339c06fa7749301870501a32938221ec85e5e
Anton Strogonoff <anton@strogonoff.name>
2021-11-17 23:37:52 +0100

    chore: use trailing slash with API endpoints

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('v1/', views.index, name='api_index'),
     path('v1/search', csrf_exempt(views.search), name='api_search'),
     re_path(
-        r'^v1/ref/(?P<dataset_name>[\w-]+)/(?P<ref>[A-z0-9\_\-\.\/]+)',
+        r'^v1/ref/(?P<dataset_name>[\w-]+)/(?P<ref>[A-z0-9\_\-\.\/]+)/',
         views.get_ref,
         name='api_get_ref'
     ),
