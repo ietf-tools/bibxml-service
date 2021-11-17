c5f99753a8b35f28c82343b42b45e3582c9b5b69
Anton Strogonoff <anton@strogonoff.name>
2021-11-17 10:31:36 +0100

    chore(urls): simplify dataset ID pattern in v1 API

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,14 @@
-from django.conf import settings
-from django.urls import include, path, re_path
+from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
-
 from . import views
 
-
-_LIBS = settings.INDEXABLE_DATASETS
-_LIBS.append("doi")
-_RE_LIBS = f'(?P<lib>{"|".join(_LIBS)})'
 
 urlpatterns = [
     path('v1/', views.index, name='api_index'),
     path('v1/search', csrf_exempt(views.search), name='api_search'),
     re_path(
-        f'^v1/ref/{_RE_LIBS}/(?P<ref>[A-z0-9\_\-\.\/]+)',
+        r'^v1/ref/(?P<dataset_name>[\w-]+)/(?P<ref>[A-z0-9\_\-\.\/]+)',
         views.get_ref,
         name='api_get_ref'
     ),
 ]
-
