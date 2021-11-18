09032b040747fcda861c37f08c29b5ce50a0291e
Anton Strogonoff <anton@strogonoff.name>
2021-11-18 16:44:57 +0100

    style: consistent whitespace after imports

--- bibxml/urls.py
+++ bibxml/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
