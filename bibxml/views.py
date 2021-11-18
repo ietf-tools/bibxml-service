a69e525ecfc2e433b2a090a3db382586f6cdc39a
Anton Strogonoff <anton@strogonoff.name>
2021-11-18 17:12:02 +0100

    chore(openapi): update legacy path per current implementation

--- bibxml/views.py
+++ bibxml/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
+from django.conf import settings
 
 
 @require_GET
 def openapi_spec(request):
-    return render(request, 'openapi.yaml')
+    return render(request, 'openapi.yaml', dict(
+        legacy_dataset_ids=list(
+            getattr(settings, 'LEGACY_DATASETS', {}).keys()),
+    ))
