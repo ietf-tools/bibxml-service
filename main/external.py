8ca2ecc2998b4dc34abd857ecf7eaea888a07ef6
Anton Strogonoff <anton@strogonoff.name>
2021-11-19 18:39:32 +0100

    chore: special case AttributeError in process_doi_list

--- main/external.py
+++ main/external.py
@@ -18,7 +18,15 @@ def get_doi_ref(ref, format='relaton'):
 
     with requests_cache.enabled():
         doi_format = 'DICT' if format == 'relaton' else 'XML'
-        doi_list = process_doi_list([ref], doi_format)
+
+        try:
+            doi_list = process_doi_list([ref], doi_format)
+        except AttributeError:
+            # Presume ref was not found.
+            raise RefNotFoundError(
+                "Reference not found (AttributeError)",
+                ref)
+
         if len(doi_list) > 0:
             # TODO: What to do with multiple DOI results for a reference?
             if format == 'relaton':
