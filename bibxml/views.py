a49b1cec0cdeb2c3461c89c29b9d64a7a98ec86e
Anton Strogonoff <anton@strogonoff.name>
2021-12-09 16:40:47 +0100

    fix: serve OpenAPI specs as text/x-yaml

--- bibxml/views.py
+++ bibxml/views.py
@@ -6,11 +6,11 @@ def openapi_spec(request):
     return render(request, 'openapi.yaml', dict(
         known_dataset_ids=list(
             getattr(settings, 'KNOWN_DATASETS', [])),
-    ))
+    ), content_type='text/x-yaml')
 
 
 def legacy_openapi_spec(request):
     return render(request, 'openapi-legacy.yaml', dict(
         legacy_dataset_ids=list(
             getattr(settings, 'LEGACY_DATASETS', {}).keys()),
-    ))
+    ), content_type='text/x-yaml')
