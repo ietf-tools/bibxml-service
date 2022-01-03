dff84ac6bc8d5fdb0b72a02e44fa73da3302b9c9
Anton Strogonoff <anton@strogonoff.name>
2022-01-03 14:58:03 +0100

    refactor: reorganize api-button.js slightly

--- static/js/api-button.js
+++ static/js/api-button.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var apiSecret = document.documentElement.dataset['api-secret'];
+  const apiSecret = document.documentElement.dataset['api-secret'];
 
   function callIndexerAPI (url, method) {
     fetch(url, {
@@ -11,22 +11,26 @@
     }).then(function () { document.location.reload() });
   }
 
+  function makeClickableButton (el) {
+    const label = el.dataset['api-action-label'];
+    const url = el.dataset['api-endpoint'];
+    const meth = el.dataset['api-method'];
+    if (url && meth) {
+      el.classList.add('link');
+      el.classList.add('dim');
+      el.style.cursor = 'pointer';
+      el.innerHTML = label;
+      el.setAttribute('role', 'button');
+      el.addEventListener('click', function () {
+        callIndexerAPI(url, meth);
+      });
+    } else {
+      console.warn("Invalid data API endpoint", el);
+    }
+  }
+
   if (apiSecret) {
-    document.querySelectorAll('[data-api-endpoint]').forEach(function (el) {
-      var label = el.dataset['api-action-label'];
-      var url = el.dataset['api-endpoint'];
-      var meth = el.dataset['api-method'];
-      if (url && meth) {
-        el.classList.add('link');
-        el.classList.add('dim');
-        el.style.cursor = 'pointer';
-        el.innerHTML = label;
-        el.setAttribute('role', 'button');
-        el.addEventListener('click', function () {
-          callIndexerAPI(url, meth);
-        });
-      }
-    });
+    document.querySelectorAll('[data-api-endpoint]').forEach(makeClickableButton);
   }
 
 })();
