df4af53ce9762b1c34829ec9573e91741a6ac498
Anton Strogonoff <anton@strogonoff.name>
2022-01-09 02:31:05 +0100

    chore(main): more type helpers/Pydantic models

---
+++ main/types.py
@@ -0,0 +1,57 @@
+"""Type helpers specific to bibliographic item retrieval."""
+
+from pydantic import BaseModel
+from typing import TypedDict, Mapping, List, Union, Optional
+from bib_models import BibliographicItem, Title, DocID as DocIDModel
+
+
+class DocID(TypedDict):
+    type: str
+    id: str
+    scope: Optional[str]
+
+
+class FoundBibliographicItem(BaseModel):
+    """Minimum bibliographic item data for search result listing."""
+
+    docid: List[DocID]
+    title: List[Title]
+    type: Union[List[str], str]
+    sources: List[str]
+
+
+class SourceMeta(BaseModel):
+    details: Optional[str] = None
+    """Extra source details, human-readable."""
+
+    home_url: Optional[str] = None
+    """Source dataset home page."""
+
+    issues_url: Optional[str] = None
+    """Where to file issues."""
+
+
+class InternalSourceMeta(SourceMeta):
+    ref: str
+    """Ref in source dataset."""
+
+
+class ExternalSourceRequest(SourceMeta):
+    """Represents a request to external source."""
+
+    time: Optional[int] = None
+    """How long the request took."""
+
+    url: Optional[str] = None
+    """Which URL was hit."""
+
+
+class ExternalSourceMeta(BaseModel):
+    requests: List[ExternalSourceRequest]
+    """Requests incurred
+    when retrieving info from this source."""
+
+
+class SourcedBibliographicItem(BibliographicItem):
+    sources: Mapping[str, Union[InternalSourceMeta, ExternalSourceMeta]]
+    """Source-specific metadata."""
