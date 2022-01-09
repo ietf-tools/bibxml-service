58e17b22ab03aaf2f6d96396fe418306776d05b8
Anton Strogonoff <anton@strogonoff.name>
2022-01-09 01:17:20 +0100

    fix(gui): exclude external datasets in management
    fix #43

--- management/views.py
+++ management/views.py
@@ -12,6 +12,10 @@ shared_context = dict(
     # NOTE: Use this context only in auth-guarded views
     api_secret=settings.API_SECRET,
     known_datasets=settings.KNOWN_DATASETS,
+    indexed_datasets=[
+        ds
+        for ds in settings.KNOWN_DATASETS
+        if ds not in settings.EXTERNAL_DATASETS],
     authoritative_datasets=settings.AUTHORITATIVE_DATASETS,
     snapshot=settings.SNAPSHOT,
 )
