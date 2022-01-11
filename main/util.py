cb410b8e4bc2e64f08bfbbc63c8b276143bceda4
Anton Strogonoff <anton@strogonoff.name>
2022-01-11 15:44:31 +0100

    perf: speed up search (WIP)

--- main/util.py
+++ main/util.py
@@ -128,7 +128,11 @@ class BaseCitationSearchView(BaseListView):
 
     def handle_json_repr_query(self, query: str) -> QuerySet[RefData]:
         quick_search = search_refs_relaton_field({
-            'keyword,docid,title': query,
+            'docid': query,
+        }, {
+            'keyword': query,
+        }, {
+            'title': query,
         })
         if len(quick_search) > 0:
             return quick_search
