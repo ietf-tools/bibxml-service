e2ed725feb073400226fc7a1505fde8f15e9cc70
Anton Strogonoff <anton@strogonoff.name>
2022-01-15 16:35:20 +0100

    chore(sources/doi): fix version in Crossref etiquette

--- sources/doi/__init__.py
+++ sources/doi/__init__.py
@@ -15,7 +15,7 @@ from main.types import SourcedBibliographicItem, ExternalSourceMeta
 
 etiquette = Etiquette(
     'BibXML service',
-    '2022.01.14_1',
+    '2022.1.14_2',
     'https://dev.bibxml.org/',
     'ietf-ribose@ribose.com',
 )
