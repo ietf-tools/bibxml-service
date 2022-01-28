f1428ad8fca37f0ac1034c0df253e39bfeeac6c9
Anton Strogonoff <anton@strogonoff.name>
2022-01-28 16:01:31 +0100

    chore: Make RefNotFoundError args optional

--- main/exceptions.py
+++ main/exceptions.py
@@ -3,6 +3,6 @@ class RefNotFoundError(RuntimeError):
 
     :param str query: Reference or query that did not yield a match."""
 
-    def __init__(self, message, query):
+    def __init__(self, message="Item not found", query="(unknown query)"):
         super().__init__(message)
         self.query = query
