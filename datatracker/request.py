b11213ed39f06efae75ac23dd97a8a69e4f623f8
Anton Strogonoff <anton@strogonoff.name>
2022-02-01 21:18:46 +0100

    docs(datatracker.request): update docstrings

--- datatracker/request.py
+++ datatracker/request.py
@@ -5,13 +5,7 @@ API_BASE = 'https://datatracker.ietf.org/api'
 
 
 def post(endpoint: str, api_key: str) -> requests.Response:
-    """Handles Datatracker POST request and unpacks JSON from response.
-
-    :raises simplejson.JSONDecodeError:
-        if JSON could not be unpacked.
-
-    :raises json.JSONDecodeError:
-        if JSON could not be unpacked and simplejson is not installed.
+    """Handles Datatracker authenticated POST request.
     """
     return requests.post(
         f'{API_BASE}/{endpoint}',
@@ -20,14 +14,7 @@ def post(endpoint: str, api_key: str) -> requests.Response:
 
 
 def get(endpoint: str, format='json') -> requests.Response:
-    """Handles Datatracker GET request, specifies JSON format
-    and unpacks JSON from response.
-
-    :raises simplejson.JSONDecodeError:
-        if JSON could not be unpacked.
-
-    :raises json.JSONDecodeError:
-        if JSON could not be unpacked and simplejson is not installed.
+    """Handles Datatracker GET request, specifies JSON format.
     """
     return requests.post(
         f'{API_BASE}/{endpoint}',
