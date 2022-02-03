8ec5ec4924bebfeae53998cb56c27ccaf739ce40
Anton Strogonoff <anton@strogonoff.name>
2022-02-03 02:31:54 +0100

    tests(main): remove unused imports

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,6 @@
 import json
-from django.test import TestCase, Client
-from django.conf import settings
+
+from django.test import TestCase
 from django.urls import reverse
 
 from .models import RefData
