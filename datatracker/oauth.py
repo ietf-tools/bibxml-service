52deb4f2d247a48a6da1ae1387c68cfeb06f2030
Anton Strogonoff <anton@strogonoff.name>
2022-02-10 22:46:22 +0100

    style(minor): break up a longer line

--- datatracker/oauth.py
+++ datatracker/oauth.py
@@ -105,7 +105,10 @@ def initiate(request):
             f"({err})")
         return redirect('/')
     else:
-        session = OAuth2Session(CLIENT_ID, scope=['openid'], redirect_uri=redirect_uri)
+        session = OAuth2Session(
+            CLIENT_ID,
+            scope=['openid'],
+            redirect_uri=redirect_uri)
         auth_url, state = session.authorization_url(
             provider.authorization_endpoint)
         request.session[OAUTH_STATE_KEY] = state
