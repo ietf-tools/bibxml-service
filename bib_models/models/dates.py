4dbc0951b37a0660dce0f7cc666ef83596b364b1
Anton Strogonoff <anton@strogonoff.name>
2022-02-15 00:22:34 +0100

    refactor: +`bib_models.models.dates.parse_date_pydantic`

--- bib_models/models/dates.py
+++ bib_models/models/dates.py
@@ -9,6 +9,7 @@ from pydantic.datetime_parse import parse_date
 __all__ = (
   'Date',
   'parse_relaxed_date',
+  'parse_date_pydantic',
   'validate_relaxed_date',
   'EXTRA_DATE_FORMATS',
 )
@@ -63,6 +64,21 @@ def parse_relaxed_date(v: str) -> Union[None, Tuple[datetime.date, str, str]]:
     return None
 
 
+def parse_date_pydantic(v) -> Union[datetime.date, None]:
+    """Parses given date or string using Pydantic’s ``parse_date()``,
+    which must return a date or raise a subclass of :class:`ValueError`.
+
+    It’s considered failed if the obtained date is the epoch
+    (which is one of Pydantic parser’s failure modes).
+    """
+    try:
+        parsed = parse_date(v)
+    except ValueError:
+        return None
+    else:
+        return parsed if parsed != datetime.date(1970, 1, 1) else None
+
+
 def validate_relaxed_date(v, optional=False):
     """To be used as validator on bibliographic item’s pydantic models
     wherever very approximate dates (no day) are possible.
@@ -77,17 +93,14 @@ def validate_relaxed_date(v, optional=False):
     if optional and v is None:
         return None
 
-    try:
-        parsed = parse_date(v)
-    except ValueError:
-        failed = True
-    else:
-        failed = parsed == datetime.date(1970, 1, 1)
+    parsed = parse_date_pydantic(v)
 
-    if failed:
+    if not parsed:
         parsed_relaxed = parse_relaxed_date(v)
+
         if parsed_relaxed is not None:
             return parsed_relaxed[1]
+
         raise ValueError(
             "Failed to parse given string "
             "even as an unspecific date")
