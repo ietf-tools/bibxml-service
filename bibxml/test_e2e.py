5f4b6b4f6dd4da6feb567bc4c877caa7f3dda704
stefanomunarini <ssteee@gmail.com>
2022-03-20 18:14:56 +0000

    Add doi ref search GUI

--- bibxml/test_e2e.py
+++ bibxml/test_e2e.py
@@ -1,4 +1,6 @@
 import os
+from urllib.parse import quote_plus
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from playwright.sync_api import sync_playwright
 
@@ -30,6 +32,22 @@ class MyViewTests(StaticLiveServerTestCase):
         self.assertEquals(page.url, '%s%s' % (
             self.live_server_url,
             '/search/?query=test&allow_format_fallback=True&bypass_cache=True',
+        ))
+
+        page.close()
+
+
+    def test_doi_ref_search(self):
+        page = self.browser.new_page()
+        page.goto(self.live_server_url)
+        ref = "10.1093/benz/9780199773787.article.b00004912"
+        page.fill('#doiRefToSearch', quote_plus(ref))
+        page.screenshot(path="test-artifacts/doi_search.png")
+        page.click('#doiRefSearchForm button[type=submit]')
+
+        self.assertEquals(page.url, '%s%s' % (
+            self.live_server_url,
+            'get-one/external/doi/?ref=+https%3A%2F%2Fdoi.org%2F10.1093%2Fbenz%2F9780199773787.article.b00004912',
         ))
 
         page.close()
