a811f3d351f4462fa530ff91b5021b9d905518d2
stefanomunarini <ssteee@gmail.com>
2022-03-22 19:29:12 +0000

    fix: restore previous values

--- xml2rfc_compat/serializer.py
+++ xml2rfc_compat/serializer.py
@@ -77,7 +77,7 @@ def to_xml(item: BibliographicItem, anchor=None) -> Element:
 
     else:
         raise ValueError(
-            "Unable to construct neither <reference> nor <referencegroup>: "
+            "Able to construct neither <reference> nor <referencegroup>: "
             "impossible combination of titles and relations")
 
     if anchor:
