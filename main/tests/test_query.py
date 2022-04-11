a7a13acef09e4f657f727f34dbaaeb5d5fa6094b
stefanomunarini <ssteee@gmail.com>
2022-04-11 19:27:05 +0100

    chore: reformat using black

--- main/tests/test_query.py
+++ main/tests/test_query.py
@@ -7,25 +7,35 @@ from django.db.models import QuerySet, Q
 
 from main.exceptions import RefNotFoundError
 from main.models import RefData
-from main.query import search_refs_relaton_field, list_refs, list_doctypes, search_refs_docids, \
-    build_citation_for_docid, build_search_results, get_indexed_item, get_indexed_ref_by_query
+from main.query import (
+    search_refs_relaton_field,
+    list_refs,
+    list_doctypes,
+    search_refs_docids,
+    build_citation_for_docid,
+    build_search_results,
+    get_indexed_item,
+    get_indexed_ref_by_query,
+)
 from main.types import CompositeSourcedBibliographicItem, IndexedBibliographicItem
 
 
 class QueryTestCase(TestCase):
-
     def setUp(self) -> None:
-        call_command('loaddata', 'xml2rfc_compat/fixtures/test_refdata.json')
+        call_command("loaddata", "xml2rfc_compat/fixtures/test_refdata.json")
 
-        with open('xml2rfc_compat/fixtures/test_refdata.json', 'r') as f:
+        with open("xml2rfc_compat/fixtures/test_refdata.json", "r") as f:
             self.json_fixtures = json.load(f)
 
     def _get_list_of_docids_from_fixture(self, dataset="rfcs"):
-        return [item["fields"]["body"]["docid"] for item
-                in self.json_fixtures if item["fields"]["dataset"] == dataset][0]
+        return [
+            item["fields"]["body"]["docid"]
+            for item in self.json_fixtures
+            if item["fields"]["dataset"] == dataset
+        ][0]
 
     def test_list_refs(self):
-        dataset_id = 'rfcs'
+        dataset_id = "rfcs"
         rfcs_refs_queryset = list_refs(dataset_id)
         self.assertGreater(rfcs_refs_queryset.count(), 0)
 
@@ -43,10 +53,13 @@ class QueryTestCase(TestCase):
         docid = next(docid["id"] for docid in docids if docid.get("scope") == "anchor")
 
         limit = 10
-        refs = search_refs_relaton_field({
-            'docid[*]': '@.id == "%s"'
-                        % re.escape(docid),
-        }, limit=limit, exact=True)
+        refs = search_refs_relaton_field(
+            {
+                "docid[*]": '@.id == "%s"' % re.escape(docid),
+            },
+            limit=limit,
+            exact=True,
+        )
         self.assertIsInstance(refs, QuerySet[RefData])
         self.assertGreater(refs.count(), 0)
         self.assertLess(refs.count(), limit)
@@ -80,10 +93,13 @@ class QueryTestCase(TestCase):
         docid = next(docid["id"] for docid in docids if docid.get("scope") == "anchor")
 
         limit = 10
-        refs = search_refs_relaton_field({
-            'docid[*]': '@.id == "%s"'
-                        % re.escape(docid),
-        }, limit=limit, exact=True)
+        refs = search_refs_relaton_field(
+            {
+                "docid[*]": '@.id == "%s"' % re.escape(docid),
+            },
+            limit=limit,
+            exact=True,
+        )
 
         found_items = build_search_results(refs)
         self.assertIsInstance(found_items, list)
@@ -91,18 +107,24 @@ class QueryTestCase(TestCase):
 
     def test_build_search_empty_results(self):
         limit = 10
-        refs = search_refs_relaton_field({
-            'docid[*]': '@.id == "%s"'
-                        % re.escape("NONEXISTENTID"),
-        }, limit=limit, exact=True)
+        refs = search_refs_relaton_field(
+            {
+                "docid[*]": '@.id == "%s"' % re.escape("NONEXISTENTID"),
+            },
+            limit=limit,
+            exact=True,
+        )
 
         found_items = build_search_results(refs)
         self.assertIsInstance(found_items, list)
         self.assertEqual(len(found_items), 0)
 
     def test_get_indexed_item(self):
-        dataset_object = [item["fields"] for item
-                          in self.json_fixtures if item["fields"]["dataset"] == "rfcs"][0]
+        dataset_object = [
+            item["fields"]
+            for item in self.json_fixtures
+            if item["fields"]["dataset"] == "rfcs"
+        ][0]
         dataset, ref = dataset_object["dataset"], dataset_object["ref"]
         indexed_item = get_indexed_item(dataset, ref)
         self.assertIsInstance(indexed_item, IndexedBibliographicItem)
@@ -113,8 +135,11 @@ class QueryTestCase(TestCase):
     #         get_indexed_item(dataset, ref)
 
     def test_get_indexed_ref_by_query(self):
-        dataset_object = [item["fields"] for item
-                          in self.json_fixtures if item["fields"]["dataset"] == "rfcs"][0]
+        dataset_object = [
+            item["fields"]
+            for item in self.json_fixtures
+            if item["fields"]["dataset"] == "rfcs"
+        ][0]
         dataset, ref = dataset_object["dataset"], dataset_object["ref"]
         data = get_indexed_ref_by_query(dataset, Q(ref__iexact=ref))
         self.assertIsInstance(data, RefData)
