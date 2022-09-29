8434b9ba64df7a0e65941777a1f171389485fdc0
Anton <anton@strogonoff.name>
2022-09-29 05:57:09 +0300

    fix(docs): update `requests` Sphinx inventory URL

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'django': ('https://docs.djangoproject.com/en/stable', 'https://docs.djangoproject.com/en/stable/_objects/'),
     'simplejson': ('https://simplejson.readthedocs.io/en/stable/', 'https://simplejson.readthedocs.io/en/stable/objects.inv'),
-    'requests': ('https://docs.python-requests.org/en/stable/', 'https://docs.python-requests.org/en/stable/objects.inv'),
+    'requests': ('https://requests.readthedocs.io/en/stable/', 'https://requests.readthedocs.io/en/stable/objects.inv'),
     'relaton': ('https://relaton-py.readthedocs.io/en/latest/', 'https://relaton-py.readthedocs.io/en/latest/objects.inv'),
 }
 
