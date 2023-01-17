ab39fbc4884b70f8d1f6a0c21a4879651ffac353
Anton <anton@strogonoff.name>
2023-01-17 11:15:23 +0800

    chore: use `Optional` instead of `Union[..., None]`

--- main/search.py
+++ main/search.py
@@ -1,6 +1,6 @@
 import re
 import json
-from typing import Any, List, Callable, Union, cast
+from typing import Any, List, Callable, Union, Optional, cast
 from urllib.parse import unquote_plus
 
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
@@ -128,7 +128,7 @@ class BaseCitationSearchView(BaseListView):
     """How long to cache search results for. Results are cached as a list
     is constructed from query and query format. Default is one hour."""
 
-    metric_counter: Union[Counter, None] = None
+    metric_counter: Optional[Counter] = None
     """A Prometheus Counter instance accepting two labels,
     ``query_format`` and ``got_results``.
     """
