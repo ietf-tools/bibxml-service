20af2234ecb51820a4567510f0133da56c507cf2
stefanomunarini <ssteee@gmail.com>
2023-09-29 11:32:46 +0100

    chore: delete SourceIndexationOutcome entries older than 10 days

--- sources/app.py
+++ sources/app.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sources'
+
+    def ready(self):
+        import sources.signals
