102338458d83f106124a4a1010317c9c57afdc8c
Sangho Na <sangho@nsh.nz>
2024-08-15 10:18:18 +1200

    revert: "refactor: Optimise count by using estimate"
    ref: 46b5d1d3dd0298a99a88571e13ebde9abab33399

--- main/views.py
+++ main/views.py
@@ -5,7 +5,6 @@ from math import log as log_, floor
 from urllib.parse import unquote_plus
 import json
 
-from django.db import connection
 from django.db.models.query import QuerySet
 from django.urls import reverse
 from django.http import QueryDict
@@ -52,22 +51,12 @@ shared_context = dict(
 """Shared context passed to GUI templates."""
 
 
-def _count_estimate(table_name: str) -> int:
-    with connection.cursor() as cursor:
-        cursor.execute(
-            'SELECT reltuples FROM pg_class WHERE relname = %s',
-            [table_name],
-        )
-        row = cursor.fetchone()
-        return int(row[0])
-
-
 def home(request):
     """Serves main landing page."""
 
     metrics.gui_home_page_hits.inc()
 
-    total_indexed_citations = _count_estimate('api_ref_data')  # Use estimate
+    total_indexed_citations = RefData.objects.count()
     units = ('', 'k', 'M', 'G', 'T', 'P')
     factor = 1000.0
     magnitude = int(floor(log_(max(abs(total_indexed_citations), 1), factor)))
