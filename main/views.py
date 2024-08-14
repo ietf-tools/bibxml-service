3cf8ab95d422333676094e5db60e21645dead777
Sangho Na <sangho@nsh.nz>
2024-08-15 10:33:56 +1200

    refactor: Optimise landing page performance (#461)
    * refactor: Mark all datasets as browsable
    
    * refactor: Optimise count by using estimate
    
    * chore: Move estimate_count function to query.py
    
    * revert: "chore: Move estimate_count function to query.py"
    
    ref: cc28c659e8706dc6c728b4c90d4b97a10bbdb551
    
    * revert: "refactor: Optimise count by using estimate"
    
    ref: 46b5d1d3dd0298a99a88571e13ebde9abab33399
    
    * refactor: Remove query for getting row count of api_ref_data

--- main/views.py
+++ main/views.py
@@ -56,28 +56,9 @@ def home(request):
 
     metrics.gui_home_page_hits.inc()
 
-    non_empty_datasets = (
-        RefData.objects.values_list('dataset', flat=True).
-        distinct())
-
-    total_indexed_citations = RefData.objects.count()
-    units = ('', 'k', 'M', 'G', 'T', 'P')
-    factor = 1000.0
-    magnitude = int(floor(log_(max(abs(total_indexed_citations), 1), factor)))
-    total_indexed_human = '%.2f%s' % (
-        total_indexed_citations / factor**magnitude,
-        units[magnitude],
-    )
-
-    browsable_datasets = [
-        ds_id
-        for ds_id in settings.RELATON_DATASETS
-        if ds_id in non_empty_datasets]
-
     return render(request, 'browse/home.html', dict(
         **shared_context,
-        total_indexed_human=total_indexed_human,
-        browsable_datasets=browsable_datasets,
+        browsable_datasets=settings.RELATON_DATASETS,
     ))
 
 
